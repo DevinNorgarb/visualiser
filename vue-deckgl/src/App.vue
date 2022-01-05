5ef6c95692c9e71fe6f73e4e409c56d61325f871
Devin Norgarb <dnorgarb@gmail.com>
2022-01-05 08:40:20 +0200

    wip

--- vue-deckgl/src/App.vue
+++ vue-deckgl/src/App.vue
@@ -41,7 +41,7 @@ export default {
       MAP_STYLE:
         "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json",
       DATA_URL:
-        "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv",
+        "http://localhost:3003/pup_with_order_processes_count.csv",
       colorRange: [
         [1, 152, 189],
         [73, 227, 206],
@@ -50,6 +50,16 @@ export default {
         [254, 173, 84],
         [209, 55, 78],
       ],
+      LIGHT_SETTINGS: {
+        lightsPosition: [
+          30, -18, 8000, 34, -19, 8000,
+        ],
+        ambientRatio: 0.4,
+        diffuseRatio: 0.6,
+        specularRatio: 0.2,
+        lightsStrength: [1, 1, 1, 1],
+        numberOfLights: 5,
+      },
       INITIAL_VIEW_STATE: {
         longitude: 18,
         latitude: -33,
@@ -57,17 +67,17 @@ export default {
         minZoom: 5,
         maxZoom: 15,
         pitch: 40.5,
-        bearing: -27,
+        bearing: 10,
       },
       mapStyle: this.MAP_STYLE,
       radius: 1000,
-      upperPercentile: 100,
+      upperPercentile: 50,
       coverage: 1,
       material: {
-        ambient: 0.64,
-        diffuse: 0.6,
-        shininess: 32,
-        specularColor: [51, 51, 51],
+        ambient: 1,
+        diffuse: 1,
+        shininess: 100,
+        specularColor: [100, 100, 100],
       },
     };
   },
@@ -110,103 +120,70 @@ export default {
     },
   },
   mounted() {
-    return this.laodFake();
-
-
-
-
+    return this.loadFake();
 
 mapboxgl.accessToken =
       "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA";
-
-
-
-
-
-
-    this.map = new mapboxgl.Map({
+          this.map = new mapboxgl.Map({
       container: "app",
       style: "mapbox://styles/mapbox/light-v10",
-      // style: 'https://tiles.devsdev.com/style.json',
       center: [18, -33],
       zoom: 9,
     });
 
-    mapboxgl.accessToken =
+this.map.on("load", () => {
+      this.loadLayer();
+  })
+  },
+
+  methods: {
+    calcElevationValue(d) {
+                  if (d.length > 1) {
+                    var orders = 0
+                    for (let index = 0; index < d.length; index++) {
+                      const element = d[index];
+                      orders += Number(element.orders);
+                    }
+                    return orders
+                  }
+              return Number(d[0].orders);
+
+    },
+    loadFake() {
+      mapboxgl.accessToken =
       "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA";
 
-    // wait for map to be ready
-    this.map.on("load", () => {
-      this.loadLayer();
-      // var deck = this.deck;
-
-      // update the layer
-      // this.deck.setProps({
-      //   layers: [],
-      // });
-    });
-  },
-  methods: {
-    laodFake() {
-      mapboxgl.accessToken =
-        "pk.eyJ1IjoicnNiYXVtYW5uIiwiYSI6ImNqNmhkZnhkZDA4M3Yyd3AwZDR4cmdhcDIifQ.TGKKAC6pPP0L-uMDJ5xFAA";
-
-      // var { MapboxLayer, HexagonLayer } = deck;
-
-      //Create the Mapbox map
       var map = new mapboxgl.Map({
-        container: document.body,
+        container: "app",
         style: "mapbox://styles/mapbox/dark-v10?optimize=true",
-        center: [-1.4157, 52.2324],
-        zoom: 6,
-        pitch: 40.5,
+        center: [ 18,-33],
+        zoom: 7,
+        pitch: 60,
         antialias: true,
+        ...this.INITIAL_VIEW_STATE
       });
 
       // Get Data for visual
-      var DATA_URL =
-        "https://tiles.devsdev.com/pup_with_order_processes_count.csv";
-        // "https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv";
-
 
       //Create the deck.gl hexagon layer and style for the data
       var OPTIONS = ["radius", "coverage", "upperPercentile"];
-      var COLOR_RANGE = [
-        [1, 152, 189],
-        [73, 227, 206],
-        [216, 254, 181],
-        [254, 237, 177],
-        [254, 173, 84],
-        [209, 55, 78],
-      ];
-      var LIGHT_SETTINGS = {
-        lightsPosition: [
-          -0.144528, 49.739968, 8000, -3.807751, 54.104682, 8000,
-        ],
-        ambientRatio: 0.4,
-        diffuseRatio: 0.6,
-        specularRatio: 0.2,
-        lightsStrength: [0.8, 0.0, 0.8, 0.0],
-        numberOfLights: 2,
-      };
 
       var hexagonLayer;
-
-      //Add the deck.gl Custom Layer to the map once the Mapbox map loads
       map.on("style.load", () => {
         hexagonLayer = new MapboxLayer({
           type: HexagonLayer,
           id: "heatmap",
-          data: require("d3").csv(DATA_URL),
+          data: require("d3").csv(this.DATA_URL),
           radius: 1000,
           coverage: 1,
           upperPercentile: 100,
-          colorRange: COLOR_RANGE,
+          colorRange: this.colorRange,
           elevationRange: [0, 1000],
           elevationScale: 250,
           extruded: true,
-          getPosition: (d) => [Number(d.lng), Number(d.lat)],
-          lightSettings: LIGHT_SETTINGS,
+          getElevationValue: (d) => this.calcElevationValue(d),
+          getPosition: (d) => [ Number(d.lng), Number(d.lat)],
+          lightSettings: this.LIGHT_SETTINGS,
           opacity: 1,
         });
 
@@ -217,68 +194,15 @@ mapboxgl.accessToken =
       // Add sliders to change the layer's settings based on user input
       OPTIONS.forEach((key) => {
         document.getElementById(key).onchange = (evt) => {
-          // var value = Number(evt.target.value);
-          // document.getElementById(key + "-value").innerHTML = value;
-          // if (hexagonLayer) {
-          //   hexagonLayer.setProps({
-          //     [key]: value,
-          //   });
-          // }
+          var value = Number(evt.target.value);
+          document.getElementById(key + "-value").innerHTML = value;
+          if (hexagonLayer) {
+            hexagonLayer.setProps({
+              [key]: value,
+            });
+          }
         };
       });
-
-      // const AIR_PORTS =
-      //   "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson";
-
-      // const deckgl = new Deck({
-      //   container: "app",
-      //   // Set your Mapbox access token here
-      //   // mapboxApiAccessToken: '<your mapbox token>',
-      //   mapboxApiAccessToken:
-      //     "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA",
-
-      //   initialViewState: {
-      //     latitude: 51.47,
-      //     longitude: 0.45,
-      //     zoom: 4,
-      //     bearing: 0,
-      //     pitch: 30,
-      //   },
-      //   controller: true,
-
-      //   layers: [
-      //     new GeoJsonLayer({
-      //       id: "airports",
-      //       data: AIR_PORTS,
-      //       // Styles
-      //       filled: true,
-      //       pointRadiusMinPixels: 2,
-      //       pointRadiusScale: 2000,
-      //       getPointRadius: (f) => 11 - f.properties.scalerank,
-      //       getFillColor: [200, 0, 80, 180],
-      //       // Interactive props
-      //       pickable: true,
-      //       autoHighlight: true,
-      //       onClick: (info) =>
-      //         info.object &&
-      //         alert(
-      //           `${info.object.properties.name} (${info.object.properties.abbrev})`
-      //         ),
-      //     }),
-      //     new ArcLayer({
-      //       id: "arcs",
-      //       data: AIR_PORTS,
-      //       dataTransform: (d) =>
-      //         d.features.filter((f) => f.properties.scalerank < 4),
-      //       // Styles
-      //       getSourcePosition: (f) => [-0.4531566, 51.4709959], // London
-      //       getTargetPosition: (f) => f.geometry.coordinates,
-      //       getSourceColor: [0, 128, 200],
-      //       getTargetColor: [200, 0, 80],
-      //       getWidth: 1,
-      //     }),
-      //   ],
-      // });
     },
     loadLayer() {
       const hexagonLayer = new HexagonLayer({
@@ -286,7 +210,7 @@ mapboxgl.accessToken =
         colorRange: this.colorRange,
         coverage: this.coverage,
         data: this.mapData,
-        elevationRange: [0, 3000],
+        elevationRange: [0, 2000],
         elevationScale: this.mapData && this.mapData.length ? 50 : 0,
         extruded: true,
         getPosition: (d) => d,
@@ -294,9 +218,8 @@ mapboxgl.accessToken =
         radius: this.radius,
         upperPercentile: this.upperPercentile,
         material: this.material,
-
         transitions: {
-          elevationScale: 3000,
+          elevationScale: 2000,
         },
       });
       this.deck = new Deck({
@@ -324,11 +247,36 @@ mapboxgl.accessToken =
 </script>
 
 <style>
-html,
+/* html,
 body,
+.mapboxgl-canvas {
+  height: 100%;
+  top: 100px;
+} */
 #app {
   margin: 0;
   width: 100%;
   height: 100%;
+}
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+}
+
+#control-panel {
+  font-family: Helvetica, Arial, sans-serif;
+  position: absolute;
+  background: #fff;
+  top: 0;
+  left: 0;
+  margin: 12px;
+  padding: 20px;
+  z-index: 1;
+}
+
+label {
+  display: inline-block;
+  width: 140px;
 }
 </style>
